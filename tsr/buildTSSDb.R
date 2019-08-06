8e9e1abf9e3551af877293fb9cbb6bc234bf724b
rtraborn <rtraborn@asu.edu>
2019-08-05 21:12:30 -0400

    Added prompt for location of buildTSSDb.

--- tsr/buildTSSDb.R
+++ tsr/buildTSSDb.R
@@ -17,8 +17,14 @@ buildTSSDb <- function(speciesNames, ...) {
 	    #print(names(my.list)) #for debugging
 	         for (i in 1:x) {
 	              this.table  <- read.table(file=x.1[[i]][1], header=TRUE)
-		      my.list[[i]] <- this.table
+		      genes.table <- this.table[complete.cases(this.table[,12]),]
+		      # lapply( #apply script here
 	      }
+
+	      ### adding gene-based assignmment to list
+
+	      test2 <- test$Pjenn[complete.cases(test$Pjenn[,12]),]   
+	      gene.list <- vector(mode="list", 
 
 	   return(my.list)
 }
