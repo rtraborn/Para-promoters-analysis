4d65da891453beeb7e392a694e9c025946335bd0
rtraborn <rtraborn@asu.edu>
2019-08-06 19:53:40 -0400

    Added beginnings of loadOrthoParalogs() script.

---
+++ scripts/loadOrthoParalogs.R
@@ -0,0 +1,12 @@
+## Parses the poff file and deposits the results in a ParaOrtho object
+
+
+loadOrthoParalogs <- function(OPtable) {
+	    if (is.character(OPtable)==FALSE) {
+	       stop("Argument OPtable must be of class 'character'")
+	       }
+	   
+### to be continued     
+	   
+	   return(my.list)
+}
