4e0f2efd595d217479158d9732afeb704a872bc8
rtraborn <rtraborn@asu.edu>
2019-08-14 18:59:50 -0400

    Completed function loadOrthoParalogs() that creates a database of ortho-paralogs by species.

--- scripts/loadOrthoParalogs.R
+++ scripts/loadOrthoParalogs.R
@@ -5,7 +5,6 @@ loadOrthoParalogs <- function(OPtable) {
 	    if (is.character(OPtable)==FALSE) {
 	       stop("Argument OPtable must be of class 'character'")
 	       }
-	   
 	   message("Importing ortho-paralog table.")
 	   myOP.table <- read.table(file=OPtable, header=TRUE)
 	   sppVec <- c("WGD_ID", "nbGenes", "Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad", "Psept", "Psex", "Pson", "Ptet", "Ptre", "Pcaud", "Pmult")
@@ -16,29 +15,27 @@ loadOrthoParalogs <- function(OPtable) {
 	   colnames(myOP.table) <- sppVec	   
 	   opList <- vector(mode="list", length=nrow(myOP.table))
 	   names(opList) <- myOP.table[,1]
-#	   	  for (i in 1:nrow(myOP.table)) {
-		  for (i in 1:10) {
+		  for (i in 1:nrow(myOP.table)) {
 		      this.id <- myOP.table[i,1]
 		      this.vec <- spp.table[i,]
 		      #print(head(this.vec))
 		      this.list <- strsplit(as.character(unlist(this.vec)), split=",")
+		      this.vector <- unlist(this.list)
 		      small.list <- vector(mode="list", length=14)
 		      names(small.list) <- sppIds
-		      opList[[i]] <- small.list
-		      for (j in 1:14) {
+		      this.ma <- matrix(this.vector, nrow=14, ncol=2, byrow=TRUE)
+		      for (j in 1:length(sppIds)) {
 		      	  this.spp <- sppIds[j]
-			  #print(this.spp)
-			  print(j)
-		      	  m.vec <- vector(mode="character", length=2)
-			  opList[[i]][j] <- m.vec
-			  this.slice <- this.list[[j]]
-			  print(this.slice)
-			  #print(length(this.slice))
-		      	  opList[[i]][j][1:2] <- this.slice
-			  #names(opList[[i]][j]) <- c("Paralog1", "Paralog2")
-			  # current status: every element of test.results.i[[i]] has only one item eg $Ppent [1] "PPENT.87.1.P0460077"
-			  # please fix this
+			  small.vec <- vector(mode="character", length=2)
+			  names(small.vec) <- c("Para1","Para2")
+			  small.list[[j]] <- small.vec
+			  #print(j)
+			  small.list[[j]] <- this.ma[j,]
+			  names(small.list[[j]]) <- c("Para1", "Para2")
+			  #print(small.vec)
+			  #print(this.ma[j,])
 			  }
+		      opList[[i]] <- small.list
 		      }
 	   
 	   return(opList)
