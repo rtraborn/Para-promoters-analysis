614fefe87f9e19beb44a0b785dbc56ede6ccbe7d
rtraborn <rtraborn@asu.edu>
2019-08-13 21:12:43 -0400

    Updates to the workflow; needs tweaking.

--- scripts/loadOrthoParalogs.R
+++ scripts/loadOrthoParalogs.R
@@ -11,18 +11,34 @@ loadOrthoParalogs <- function(OPtable) {
 	   sppVec <- c("WGD_ID", "nbGenes", "Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad", "Psept", "Psex", "Pson", "Ptet", "Ptre", "Pcaud", "Pmult")
 	   colnames(myOP.table) <- sppVec
 	   spp.table <- myOP.table[,-1:-2]
-	   sppIds <- c("Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad", "Psept", "Psex", "Pson", "Ptet", "Ptre", "Pcaud", "Pmult")
+	   spp.table <- spp.table[,-15:-16]
+	   sppIds <- c("Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad", "Psept", "Psex", "Pson", "Ptet", "Ptre")
 	   colnames(myOP.table) <- sppVec	   
 	   opList <- vector(mode="list", length=nrow(myOP.table))
 	   names(opList) <- myOP.table[,1]
-	   	  for (i in 1:nrow(myOP.table)) {
+#	   	  for (i in 1:nrow(myOP.table)) {
+		  for (i in 1:10) {
 		      this.id <- myOP.table[i,1]
-		      #print(this.id)
-		      this.slot <- spp.table[i,]
-#		      this.vec <- unlist(as.character(spp.table[i,]))
-		      opList[[i]] <- this.slot
-		      ### need to work on assigning each paralog
-		      ### remove multimicronuc
+		      this.vec <- spp.table[i,]
+		      #print(head(this.vec))
+		      this.list <- strsplit(as.character(unlist(this.vec)), split=",")
+		      small.list <- vector(mode="list", length=14)
+		      names(small.list) <- sppIds
+		      opList[[i]] <- small.list
+		      for (j in 1:14) {
+		      	  this.spp <- sppIds[j]
+			  #print(this.spp)
+			  print(j)
+		      	  m.vec <- vector(mode="character", length=2)
+			  opList[[i]][j] <- m.vec
+			  this.slice <- this.list[[j]]
+			  print(this.slice)
+			  #print(length(this.slice))
+		      	  opList[[i]][j][1:2] <- this.slice
+			  #names(opList[[i]][j]) <- c("Paralog1", "Paralog2")
+			  # current status: every element of test.results.i[[i]] has only one item eg $Ppent [1] "PPENT.87.1.P0460077"
+			  # please fix this
+			  }
 		      }
 	   
 	   return(opList)
