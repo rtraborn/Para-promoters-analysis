88b3951e650c1b8403cccbce22db618612e95cb3
rtraborn <rtraborn@asu.edu>
2019-08-19 21:12:53 -0400

    Completed most of a script that determines the number of paralogs in each ortho-paralog group.

---
+++ scripts/evalParaGroups.R
@@ -0,0 +1,42 @@
+## Parses an OPobj and returns a matrix containing information about the number of paralogs in each ortho-paralog group
+
+
+evalParaGroups <- function(OPobj) {
+	   message("Calculating ortho-paralog membership.")
+	   mySpp <- c("Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad" , "Psept", "Psex", "Pson", "Ptet", "Ptre")
+	   this.ma1 <- matrix(NA, nrow=length(OPobj), ncol=14)
+	   rownames(this.ma1) <- names(OPobj)
+	   colnames(this.ma1) <- c("Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad" , "Psept", "Psex", "Pson", "Ptet", "Ptre")
+	   this.ma2 <- matrix(NA, nrow=length(OPobj), ncol=14)
+	   colnames(this.ma2) <- c("Pbi", "Pdec", "Pdodec", "Pjenn", "Pnov", "Poct", "Ppent", "Pprim", "Pquad" , "Psept", "Psex", "Pson", "Ptet", "Ptre")
+	   rownames(this.ma2) <- names(OPobj)
+
+#		for (i in 1:length(OPobj)) {
+		for (i in 1:10) {
+	       	   spp.list <- OPobj$i
+		   thisOP <- spp.list[[1]]	       	   
+
+	       	   for (j in mySpp) {
+		       my.vec <- thisOP$mySpp
+		       if (my.vec[1] == "." && my.vec[2] == ".") { #fix this logical
+		       	  n.total <- 0
+			  this.ma1[i,j] <- n.total
+			  next
+			  }
+		       if (my.vec[1] == "." || my.vec[2] == ".") { #fix this logical
+		       	  n.total <- 1
+			  this.ma1[i,j] <- n.total
+			  next
+			  }
+		       else {
+		          n.total <- 2
+			  this.ma1[i,j] <- n.total
+			  }
+	   	    }  
+		}
+			  
+	   return(this.ma1)
+}
+
+# current error message: Error in if (my.vec[1] == "." && my.vec[2] == ".") { :
+# missing value where TRUE/FALSE needed
