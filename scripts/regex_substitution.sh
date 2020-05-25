5f7cf83c287cc42ad0f484e8038b1f13f995e8da
rtraborn <rtraborn@asu.edu>
2020-05-25 16:10:37 -0700

    Updated regex for this file.

--- scripts/regex_substitution.sh
+++ scripts/regex_substitution.sh
@@ -9,15 +9,15 @@ cd $myDir
 #TODO there are still remaining instances with ",", please fix
 
 perl -pe 's/(\sNA)(\,)(NA)/\1\t\3/g' all_aurelias.poff > tmp.1
-perl -pe 's/(\w+\.\S+\.\d+\.P\d+)(\,)(\w+\.\S+\.\d+\.P\d+)/\1\t\3/g' tmp.1 > tmp.2
+perl -pe 's/(\w+\.\S+\.\S+\.P\d+)(\,)(\w+\.\S+\.\S+\.P\d+)/\1\t\3/g' tmp.1 > tmp.2
 perl -pe 's/(\s\.)(\,)(\w+\.\S+\.\d+\.P\d+)/\1\t\3/g' tmp.2 > tmp.3
 perl -pe 's/(\s\w+\.\S+\.\d+\.P\d+)(\,)(\.)/\1\t\3/g' tmp.3 > tmp.4
+perl -pe 's/(\.)(\,)(\.)/\1\t\3/g' tmp.4 > tmp.5
 
 #now for the annoying PPRIM ids:
-perl -pe 's/(\s\w+\.\S+\.P\d+)(\,)(\.)/\1\t\3/g' tmp.4 > tmp.5
 perl -pe 's/(\sPPRIMP\d+)(\,)(\.)/\1\t\3/g' tmp.5 > tmp.6
 perl -pe 's/(\.)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.6 > tmp.7
-perl -pe 's/(\sPPRIMP\d+)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.7 > tmp.8
-perl -pe 's/(\.)(\,)(\.)/\1\t\3/g' tmp.8 > $outFile 
+perl -pe 's/(\sPPRIMP\d+)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.7 > $outFile
+
 
 echo "Job complete."
