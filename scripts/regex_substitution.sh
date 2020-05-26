5ece41a0b1033e960ab8bc679737494ca64bcf0a
rtraborn <rtraborn@asu.edu>
2020-05-26 15:56:42 -0700

    Current script leaves only three commas.

--- scripts/regex_substitution.sh
+++ scripts/regex_substitution.sh
@@ -14,10 +14,18 @@ perl -pe 's/(\s\.)(\,)(\w+\.\S+\.\d+\.P\d+)/\1\t\3/g' tmp.2 > tmp.3
 perl -pe 's/(\s\w+\.\S+\.\d+\.P\d+)(\,)(\.)/\1\t\3/g' tmp.3 > tmp.4
 perl -pe 's/(\.)(\,)(\.)/\1\t\3/g' tmp.4 > tmp.5
 
+echo "Made it to tmp.5"
+perl -pe 's/(\s\w+\.\S+\.P\d+)(\,)(\w+\.\S+\.P\d+)/\1\t\3/g' tmp.5 > tmp.6
+echo "Made it to tmp.6"
+perl -pe 's/(\s\.)(\,)(\w+\.\S+\.P\d+)/\1\t\3/g' tmp.6 > tmp.7
+echo "Made it to tmp.7"
+perl -pe 's/(\s\w+\.\S+\.P\d+)(\,)(\.)/\1\t\3/g' tmp.7 > tmp.8
+echo "Made it to tmp.8"
+
 #now for the annoying PPRIM ids:
-perl -pe 's/(\sPPRIMP\d+)(\,)(\.)/\1\t\3/g' tmp.5 > tmp.6
-perl -pe 's/(\.)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.6 > tmp.7
-perl -pe 's/(\sPPRIMP\d+)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.7 > $outFile
+perl -pe 's/(\sPPRIMP\d+)(\,)(\.)/\1\t\3/g' tmp.8 > tmp.9
+perl -pe 's/(\.)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.9 > tmp.10
+perl -pe 's/(\sPPRIMP\d+)(\,)(PPRIMP\d+)/\1\t\3/g' tmp.10 > $outFile
 
 
 echo "Job complete."
