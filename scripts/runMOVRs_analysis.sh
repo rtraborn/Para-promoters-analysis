64edf9a04fcf1386a34c3f4a06e5d1c13ed22868
rtraborn <rtraborn@asu.edu>
2020-08-11 14:03:38 -0400

    Added first runMoVRs script.

---
+++ scripts/runMOVRs_analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -N RunMoVRs_test
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=64gb
+#PBS -l walltime=24:00:00
+
+echo "Launching MoVRs job"
+
+cd /N/dc2/projects/ParameciumPromoters/Para-promoters-analysis/
+
+/N/u/rtraborn/Carbonate/genome_analysis/MoVRs/scripts/MoVRs -f fasta_files/PdSTRIPE_TATA.fasta -b Pdec_TATA_bg.fasta -S 10 -k 3 -p 9 --outputdir Pdec_TATAtest >& errTEST
+
+echo "Job complete"
