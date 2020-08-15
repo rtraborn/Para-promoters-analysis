1c7c9cd661a8ab0f0eaea001dc51a97bd498bb3f
rtraborn <rtraborn@asu.edu>
2020-08-15 16:40:00 -0700

    Added Agave subsmission script for the MOVRs test.

---
+++ scripts/runMOVRs_analysis_Agave.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p mrline-serial
+#SBATCH -n 24
+#SBATCH -t 08:00:00
+
+module purge
+
+echo "Launching MoVRs job"
+
+cd /home/rraborn//scratch/Para-promoters-analysis/
+
+/home/rraborn/genome_analysis/MoVRs/scripts/MoVRs -f fasta_files/PdSTRIPE_TATA.fasta -b Pdec_TATA_bg.fasta -S 10 -k 3 -p 9 --outputdir Pdec_TATAtest >& errTEST
+
+echo "Job complete"
