7f8b72ca6ffd4236336288c9aae5fefcda643b25
rtraborn <rtraborn@asu.edu>
2020-08-16 17:01:38 -0700

    Created submission scripts to run on Agave.

---
+++ scripts/runMOVRs_analysis_Pnov_agave.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p serial
+#SBATCH -n 24
+#SBATCH -t 12:00:00
+
+echo "Launching MoVRs job"
+
+module load blast/2.9.0  
+
+fastaFile=/home/rraborn/scratch/assemblies/pnov_mod.fa 
+peakFile=/home/rraborn/scratch/Para-promoters-analysis/bed_files/PnSTRIPE_inr.pos
+
+cd /home/rraborn/scratch/Para-promoters-analysis/
+
+MoVRs -a $peakFile -G Pnov -o Pnov_60_40 --size [-60,40] -k 10 -p 10 >& errPNOV_6040
+
+echo "Job complete"
