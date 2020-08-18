2c3fe5d46d1acab2cff79b8c0fdde0b5725d2bd0
rtraborn <rtraborn@asu.edu>
2020-08-17 20:32:35 -0400

    Added updated scripts using new pos files.

---
+++ scripts/runMOVRs_analysis_Pdec_ii.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PBS -N RunMoVRs_Pdec
+#PBS -k o
+#PBS -l nodes=1:ppn=20,vmem=64gb
+#PBS -l walltime=8:00:00
+
+echo "Launching MoVRs job"
+
+module load meme
+module load blat
+module load blast
+
+fastaFile=/N/dc2/projects/ParameciumPromoters/assemblies/pdec_mod.fa 
+peakFile=/N/dc2/projects/ParameciumPromoters/Para-promoters-analysis/bed_files/PdTSS.pos
+
+cd /N/dc2/projects/ParameciumPromoters/Para-promoters-analysis/
+
+MoVRs -a $peakFile -G Pdec -o Pjen_60_40_ii --size [-60,40] -l 6,8,10 -k 5 -p 20 >& errPDEC_6040_ii
+
+echo "Job complete"
