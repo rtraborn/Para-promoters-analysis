8d47986a0934c0ce393928a01cdeaeb435f5daa7
rtraborn <rtraborn@asu.edu>
2020-08-26 11:35:57 -0700

    Updated poff file import script.

--- scripts/poff_file_import.R
+++ scripts/poff_file_import.R
@@ -2,7 +2,9 @@
 setwd("/scratch/rraborn/Para-promoters-analysis/paralog_datasets/")
 
 poff <- read.table(file="Poff_table_updated_082020.txt", header=TRUE)
-poff_new <- cbind(poff[,1], poff2)
+poff2 <- poff[,-1:-4]
+poff3 <- poff2[,-27]
+poff_new <- cbind(poff[,1], poff3)
 
 col1 <- "geneFamID"
 col2 <- colnames(poff3)
