5d55c3423b5dcedef367688d258b465623c5624f
rtraborn <rtraborn@asu.edu>
2020-12-11 13:42:54 -0700

    Added script to create .genome files for bedtools operations.

--- scripts/update_geneIDs.sh
+++ scripts/update_geneIDs.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/sh
 
-WD=/home/rraborn/scratch/Para-promoters-analysis/tsr/MergedTSR_files
-file1=Pprim_TSRsetMerged.txt
+WD=/home/rraborn/Paramecium_data/assemblies
 
 cd $WD
 
-for file in *_TSRsetMerged.txt; do
-echo "Processing "
+for file in *.fa.fai
+do
+echo "Processing"
 echo $file
-cut -f 12 $file | perl -pi.bak -e 's/^(\w+\.\S+\.)(\d\.)G(\w\d+$)/\1\2P\3/' > ${file}_col12.txt
-cut -f 1-11 $file > ${file}_col1_11.txt
-paste ${file}_col1_11.txt ${file}_col12.txt > $(basename $file .txt)_updated.txt
+cut -f1-2 $file > $(basename $file .fa.fai).genome
 done
-
-echo "Processing Pprim, which is unusual."
-echo $file1
-
-cut -f 12 $file1 | perl -pi.bak -e 's/^(PPRIM)(G)(\d+$)/\1P\3/' | head
-cut -f 12 $file1 | perl -pi.bak -e 's/^(PPRIM)(G)(\d+$)/\1P\3/' > ${file1}_col12.txt
-cut -f 1-11 $file1 > ${file1}_col1_11.txt
-paste ${file1}_col1_11.txt ${file1}_col12.txt > $(basename $file1 .txt)_updated.txt
 
 echo "Job complete"
