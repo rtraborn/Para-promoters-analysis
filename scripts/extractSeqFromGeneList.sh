7a47ba1e1336379259001f89ff75132911570991
rtraborn <rtraborn@asu.edu>
2021-03-03 11:53:30 -0700

    made small updates to extractSeq function, which is still in progress.

--- scripts/extractSeqFromGeneList.sh
+++ scripts/extractSeqFromGeneList.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/sh
 
 #filesList is a config file that contains all of the Paramecium annotations and the associated paths
-source /N/project/ParameciumPromoters/Para-promoters-analysis/scripts/filesList
 
-outDir=/N/project/ParameciumPromoters/Para-promoters-analysis/paralogon
-geneList=/N/project/ParameciumPromoters/Para-promoters-analysis/paralogon/98_genelist_updated.txt
-Bedtools=/N/soft/rhel7/bedtools/gnu/2.26.0/bin/bedtools
+### if using Carbonate
+#source /N/project/ParameciumPromoters/Para-promoters-analysis/scripts/filesList
+
+#outDir=/N/project/ParameciumPromoters/Para-promoters-analysis/paralogon
+#geneList=/N/project/ParameciumPromoters/Para-promoters-analysis/paralogon/98_genelist_updated.txt
+#Bedtools=/N/soft/rhel7/bedtools/gnu/2.26.0/bin/bedtools
+
+### if using Agave
+source /home/rraborn/Para-promoters-analysis/scripts/filesList
+
+outDir=/home/rraborn/Para-promoters-analysis/paralogon
+geneList=/home/rraborn/Para-promoters-analysis/paralogon/98_genelist_updated.txt
+Bedtools=/packages/7x/bedtools/2.17.0/bin/bedtools
 
 cd $outDir
 
@@ -32,14 +41,18 @@ done < $geneList
     mkdir out_files
     split -l 1 --additional-suffix=.bed test.out out_files/98_genelist_
 
-    for b in *_a*.bed; do
+#    for b in *_a*.bed; do
 #	$Bedtools -i $b #this needs to be the bedtools flank command, which recognizes the individual -genome file
-	done
+#	done
 
     for b in *_a*.bed; do
 	bedtools subtract -s -a $(basename $i .bed)_flank.bed -b $i > $(basename $i _flank.bed)_promoter.bed;
     done
-    for p in *_promoter.bed; do
-	bedtools getfasta
+
+#### need to select the appropriate fasta file for the species in question
+for p in *_promoter.bed; do
+	bedtools getfasta -s -fi   -bed $(basename $i _flank.bed)_promoter.bed -fo $(basename $i _flank.bed)_promoter.fasta
     
 done
+
+### add prank alignment step in subsequent command
