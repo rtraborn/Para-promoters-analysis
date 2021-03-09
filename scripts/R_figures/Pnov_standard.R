e12a2b1c610b7bed1146a5bf704ea91e626405df
rtraborn <rtraborn@asu.edu>
2021-03-09 13:58:55 -0700

    Tweaked and updated code.

--- scripts/R_figures/Pnov_standard.R
+++ scripts/R_figures/Pnov_standard.R
@@ -1,3 +1,5 @@
+## P. novaurelia
+
 ### TSRexploreR standard processing module
 
 setwd("/home/rraborn/Para-promoters-analysis/tss") 
@@ -8,29 +10,27 @@ library(viridis)
 library(ggrastr)
 library(ggplot2)
 
-#load("PdSTRIPE_complete.RData")
-#load("PdSTRIPE_testObj.RData")
-load("PdSTRIPE_testObj_tsr.RData")
-load("PdSTRIPE_testObj_tss.RData")
 
-#Pd.annot <- "/home/rraborn/scratch/annotations/pdec_cds.gff"
-Pd.annot <- "/home/rraborn/scratch/annotations/pdec-full.gff"
-Pd.assembly <- "/home/rraborn/scratch/assemblies/pdec_mod.fa"
+
+
+Pn.annot <- "/home/rraborn/scratch/annotations/pnov-full.gff"
+Pn.assembly <- "/home/rraborn/scratch/assemblies/pnov_mod.fa"
 #Pd.assembly.short <- "/home/rraborn/scratch/assemblies/pdec_scaf_1_10.fa"
-Pd_pos <- "/home/rraborn/scratch/Para-promoters-analysis/tss"
 
 # Generate sample sheet
 sample_sheet <- data.frame(
-  sample_name=stringr::str_glue("Pdec_{seq_len(3)}"),
+  sample_name=stringr::str_glue("Pnov_{seq_len(3)}"),
   file_1=NA, file_2=NA,
   condition=rep("Vegetative", 3)
 )
 
+tss.1 <- read.table(file="Pn_TSSset-1.txt", header=TRUE)
+tss.2 <- read.table(file="Pn_TSSset-2.txt", header=TRUE)
+tss.3 <- read.table(file="Pn_TSSset-3.txt", header=TRUE)
 
-#writing the tss files to the workspace
-#tss.1 <- PdSTRIPE@tssCountData[[1]]
-#tss.2 <- PdSTRIPE@tssCountData[[2]]
-#tss.3 <- PdSTRIPE@tssCountData[[3]]
+tsr.1 <- read.table(file="/home/rraborn/Para-promoters-analysis/tsr/Pn_TSRset-1.tab", header=FALSE)
+tsr.2 <- read.table(file="/home/rraborn/Para-promoters-analysis/tsr/Pn_TSRset-2.tab", header=FALSE)
+tsr.3 <- read.table(file="/home/rraborn/Para-promoters-analysis/tsr/Pn_TSRset-3.tab", header=FALSE)
 
 colnames(tss.1) <- c("seq","TSS", "strand", "score")
 colnames(tss.2) <- c("seq","TSS", "strand", "score")
@@ -61,11 +61,10 @@ tss.3.gr <- makeGRangesFromDataFrame(tss.3,
                                      strand.field = "strand"
 )
 
-Pd.tss <- list(tss.1.gr, tss.2.gr, tss.3.gr)
-names(Pd.tss) <- c("Pdec_1", "Pdec_2", "Pdec_3")
+Pn.tss <- list(tss.1.gr, tss.2.gr, tss.3.gr)
+names(Pn.tss) <- c("Pnov_1", "Pnov_2", "Pnov_3")
 
-Pd.tsr <- list(tsr.1.gr, tsr.2.gr, tsr.3.gr)
-names(Pd.tsr) <- c("Pdec_1", "Pdec_2", "Pdec_3")
+
 
 #making granges files from tsr data frames
 tsr.1.gr <- makeGRangesFromDataFrame(tsr.1,
@@ -92,11 +91,19 @@ tsr.3.gr <- makeGRangesFromDataFrame(tsr.3,
                                      strand.field = "strand"
 )
 
+Pn.tsr <- list(tsr.1.gr, tsr.2.gr, tsr.3.gr)
+names(Pd.tsr) <- c("Pnov_1", "Pnov_2", "Pnov_3")
+
 #Creating the TSR explorer object
-exp <- tsr_explorer(TSSs=Pd.tss, TSRs=Pd.tsr, 
-                    genome_annotation=Pd.annot, genome_assembly=Pd.assembly,
+exp <- tsr_explorer(TSSs=Pn.tss, 
+                    genome_annotation=Pn.annot, genome_assembly=Pn.assembly,
                     sample_sheet = sample_sheet
 )
+
+#exp <- tsr_explorer(TSSs=Pn.tss, TSRs=Pn.tsr, 
+#                    genome_annotation=Pn.annot, genome_assembly=Pn.assembly,
+#                    sample_sheet = sample_sheet
+#)
 
 #Initial TSS processing
 
@@ -118,14 +125,14 @@ exp <- annotate_features(exp, data_type = "tss", feature_type="transcript")
 #exp <- format_counts(exp, data_type="tss")
 #exp <- normalize_counts(exp, data_type = "tss", method = "DESeq2")
 
-#plot_threshold_exploration(exp, samples="Pdec_1", point_size=1) + scale_color_viridis_c()
-#plot_threshold_exploration(exp, samples="Pdec_2", point_size=1) + scale_color_viridis_c()
-#plot_threshold_exploration(exp, samples="Pdec_3", point_size=1) + scale_color_viridis_c()
+plot_threshold_exploration(exp, samples="Pnov_1", point_size=1) + scale_color_viridis_c()
+plot_threshold_exploration(exp, samples="Pnov_2", point_size=1) + scale_color_viridis_c()
+plot_threshold_exploration(exp, samples="Pnov_3", point_size=1) + scale_color_viridis_c()
 
 #plotting sequence logo for all three replicates
-plot_sequence_logo(exp, samples="Pdec_1")
-plot_sequence_logo(exp, samples="Pdec_2")
-plot_sequence_logo(exp, samples="Pdec_3")
+plot_sequence_logo(exp, samples="Pnov_1")
+plot_sequence_logo(exp, samples="Pnov_2")
+plot_sequence_logo(exp, samples="Pnov_3")
 
 ### plot correlation
 
@@ -135,35 +142,35 @@ plot_correlation(
   heatmap_colors=viridis::viridis(100)
 )
 
-ggsave(file="Pdec_correlation_matrix.png") #saving the plot
+ggsave(file="Pnov_correlation_matrix.png") #saving the plot
 
 ### plot genomic distribution of TSSs
 
-plot_genomic_distribution(exp, data_type="tss", samples=c("Pdec_1","Pdec_2","Pdec_3")) +
+plot_genomic_distribution(exp, data_type="tss", samples=c("Pnov_1","Pnov_2","Pnov_3")) +
   scale_fill_viridis_d(direction=-1, name="Annotation") 
 
-ggave(file="Pdec_genomic_distribution.png") #saving the plot
+ggsave(file="Pnov_genomic_distribution.png") #saving the plot
 
 ### plot density
 
-plot_density(exp, data_type="tss", samples=c("Pdec_1", "Pdec_2", "Pdec_3"))
+plot_density(exp, data_type="tss", samples=c("Pnov_1", "Pnov_2", "Pnov_3"))
 
 #Current error
 #Error in `[.data.table`(sample_data, , `:=`(samples, factor(samples, levels = samples))) : 
 #Supplied 3 items to be assigned to 384133 items of column 'samples'. If you wish to 'recycle' the RHS please use rep() to make this intent clear to readers of your code.
 
-ggsave(file="Pdec_density.png") #saving the plot
+ggsave(file="Pnov_density.png") #saving the plot
 
 ### plot TSS heatmap
 
 plot_heatmap(
-  exp, data_type="tss", samples=c("Pdec_1","Pdec_2","Pdec_3"),
+  exp, data_type="tss", samples=c("Pnov_1","Pnov_2","Pnov_3"),
   upstream=250, downstream=250,
   use_normalized=TRUE,
   rasterize=TRUE, raster_dpi=150
 )
 
-ggave(file="Pdec_tss_heatmap.png") #saving the plot
+ggsave(file="Pnov_tss_heatmap.png") #saving the plot
 
 ### formatting/normalizing tsrs
 exp <- format_counts(exp, data_type="tsr")
@@ -175,10 +182,10 @@ exp <- annotate_features(exp, data_type = "tsr", feature_type="gene")
 ## this doesn't work yet
 
 plot_heatmap(
-  exp, data_type="tsr", samples=c("Pdec_1","Pdec_2","Pdec_3"),
+  exp, data_type="tsr", samples=c("Pnov_1","Pnov_2","Pnov_3"),
   upstream=250, downstream=250,
   use_normalized=TRUE,
   rasterize=TRUE, raster_dpi=150
 )
 
-ggave(file="Pdec_tsr_heatmap.png") #saving the plot
+ggave(file="Pnov_tsr_heatmap.png") #saving the plot
