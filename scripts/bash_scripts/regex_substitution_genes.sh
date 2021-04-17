fe9dd35f287dde768ba55f71689c6c419bdf6d95
rtraborn <rtraborn@asu.edu>
2021-04-16 17:17:21 -0700

    Added updated regex conversion script for genes.

--- scripts/bash_scripts/regex_substitution_genes.sh
+++ scripts/bash_scripts/regex_substitution_genes.sh
@@ -7,9 +7,9 @@ outFile=ParaGenes_ids.gff
 cd $myDir
 
 #replacing most of the ids
-perl -pe 's/(\w+\.\S+\.\S+\.P\d+)(\,)(\w+\.\S+\.\S+\.P\d+)/\1\t\3/g' $fileName > tmp
+perl -pe 's/(\d+\.)(G)(\d+)/\1\P\3/g' $fileName > tmp
 
 #now for the annoying PPRIM ids:
-perl -pe 's/(\sPPRIMP\d+)/\1\t\3/g' tmp > $outFile
+perl -pe 's/(PPRIM)(G)(\d+)/\1\P\3/g' tmp > $outFile
 
 echo "Job complete."
