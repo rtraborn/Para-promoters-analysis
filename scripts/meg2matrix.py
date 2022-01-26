7899f96acd813955920215416fff9b7e52cd861a
rtraborn <rtraborn@asu.edu>
2022-01-26 14:14:22 -0700

    Major improvements to meg2matrix.py; to parse matrix next

--- scripts/meg2matrix.py
+++ scripts/meg2matrix.py
@@ -14,23 +14,34 @@ def meg2matrix():
     """Converts mega output to a parsable matrix format"""
     print("Starting mega")
 
-
+nTaxa = []
 myOutput = open(args.mega_data, "r")
-
+regex = r"(No. of Taxa:)(\d+)"
 lines = myOutput.readlines()
-geneData = []
-matData = []
-#str = r'\;'
-str = r'\[d+ \]' #TODO need to fix this regex
-new_file = []
 for x in lines:
-    match = re.search(str, x, re.MULTILINE)
-    if match:
-        new_line = match.group() + '\n'
-        new_file.append(new_line)
-        print(x)
+    if re.search(regex, x):
+        mySplit=x.split(":")
+        nTaxa=mySplit[1]
     myOutput.close()
 
-print(new_file)
+n_taxa=int(nTaxa)
+geneData = []
+matData = []
+endPoint=34+n_taxa
+print(endPoint)
 
+print("The number of taxa is:", nTaxa)
+
+#obtaining the geneID list
+for line in lines[34:endPoint]:
+    geneSplit=line.split("#")
+    geneID=geneSplit[1]
+    geneData.append(geneID)
+    myOutput.close()
+
+
+#for line in lines[newStart:newEnd]:
+    #### the same thing but for the matrix data
+
+print(geneData)
 print("Mega parser complete!")
